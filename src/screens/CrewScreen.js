194fea73f4f2d6c45c78f0799ea1756aacc9e14c
lokesh <lokeshrajeshbabu28@gmail.com>
2023-12-18 22:13:24 +0530

    added screens

--- src/screens/CrewScreen.js
+++ src/screens/CrewScreen.js
@@ -3,15 +3,28 @@ import { Card } from '@rneui/themed';
 import React, { useEffect, useState } from 'react';
 import { ActivityIndicator, FlatList, Image, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
+const CrewCard = ({ item, handleImagePress }) => {
+    return (
+        <Card style={styles.card}>
+            <TouchableOpacity onPress={() => handleImagePress(item)}>
+                <View>
+                    <Text style={styles.text2}>{item.name}</Text>
+                    <Image style={styles.image} source={{ uri: item.image }} />
+                </View>
+            </TouchableOpacity>
+        </Card>
+    );
+};
+
 const CrewScreen = () => {
     const navigation = useNavigation();
     const [isLoading, setLoading] = useState(true);
     const [data, setData] = useState([]);
 
     const handleImagePress = (item) => {
-        // Navigate to CrewDetailScreen and pass necessary data, e.g., item ID
         navigation.navigate('CrewDetailScreen', { id: item.id });
     };
+
     useEffect(() => {
         fetch('https://api.spacexdata.com/v4/crew')
             .then((response) => response.json())
@@ -22,34 +35,20 @@ const CrewScreen = () => {
             .catch((error) => console.error(error));
     }, []);
 
-
-
     return (
         <SafeAreaView>
             <View style={styles.container}>
-                <Card>
-                    {isLoading ? (
-                        <ActivityIndicator />
-                    ) : (
-                        <View>
-                            <FlatList
-                                data={data}
-                                renderItem={({ item }) => (
-                                    <TouchableOpacity key={item.id} onPress={() => handleImagePress(item)}>
-                                        <View>
-                                            <Text style={styles.text2}>{item.name}</Text>
-                                            <Image style={styles.image} source={{ uri: item.image }} />
-                                        </View>
-                                    </TouchableOpacity>
-                                )}
-                                keyExtractor={(item) => item.id.toString()}
-                            />
-                        </View>
-                    )}
-                </Card>
+                {isLoading ? (
+                    <ActivityIndicator />
+                ) : (
+                    <FlatList
+                        data={data}
+                        renderItem={({ item }) => <CrewCard item={item} handleImagePress={handleImagePress} />}
+                        keyExtractor={(item) => item.id.toString()}
+                    />
+                )}
             </View>
         </SafeAreaView>
-
     );
 };
 
@@ -62,12 +61,15 @@ const styles = StyleSheet.create({
     text2: {
         fontSize: 28,
         color: '#2e8b57'
-
     },
     container: {
         marginHorizontal: 2,
-        backgroundColor: '#e0ffff'
-
+    },
+    card: {
+        marginVertical: 10,
+        marginHorizontal: 5,
+        borderRadius: 10,
+        elevation: 4,
     },
 });
 
